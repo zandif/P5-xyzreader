118272bebe941446f62a8d8ca327343d4a6821b0
zandif <skylance@gmail.com>
2016-02-07 15:46:58 -0700

    Adjusted scrolling behavior, accent color, and updated okhttp
    Scrolling behavior from:
    http://developer.android.com/reference/android/support/design/widget/AppBarLayout.html
    Accent color now matches icon
    okhttp 1.0 -> 3.01

--- XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
@@ -2,17 +2,19 @@ package com.example.xyzreader.remote;
 
 import android.util.Log;
 
-import com.squareup.okhttp.OkHttpClient;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONTokener;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class RemoteEndpointUtil {
     private static final String TAG = "RemoteEndpointUtil";
@@ -48,14 +50,18 @@ public class RemoteEndpointUtil {
         return new String(fetch(url), "UTF-8" );
     }
 
+    // Updated fetch from https://gist.github.com/popnfresh234/30a508863667300483a7
     static byte[] fetch(URL url) throws IOException {
         InputStream in = null;
 
         try {
             OkHttpClient client = new OkHttpClient();
-            HttpURLConnection conn = client.open(url);
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            Response response = client.newCall(request).execute();
             ByteArrayOutputStream out = new ByteArrayOutputStream();
-            in = conn.getInputStream();
+            in = new ByteArrayInputStream(response.body().string().getBytes());
             byte[] buffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = in.read(buffer)) > 0) {
